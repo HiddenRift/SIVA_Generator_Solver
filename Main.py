ab15035147b2da8533b168995c52b1fde53ffb45
Robert Fry <robert3.fry@live.uwe.ac.uk>
2017-01-10 15:15:42 +0000

    added test_column_values function, need to test

--- Main.py
+++ Main.py
@@ -11,6 +11,8 @@ def main():
     column4 = [1, 2, 3, -4, -5]
     column5 = [1, 2, 3, -4, -5]
 
+    test_column_values(1, column1)
+
     for num1 in column1:
         for num2 in column2:
             for num3 in column3:
@@ -19,6 +21,15 @@ def main():
                         if calc_numbers(start_value, num1, num2, num3, num4, num5) == end_value:
                             print(str(num1), str(num2), str(num3), str(num4), str(num5), "equals", str(end_value))
     return
+
+
+def test_column_values(column_number, column):
+    for i in range(0, 3):
+        if column[i] < 0:
+            print("Error in", str(column_number), ", Element", str(i))
+    for i in range(3, 5):
+        if column[i] >= 0:
+            print("Error in", str(column_number), ", Element", str(i))
 
 
 def calc_numbers(start, num1, num2, num3, num4, num5):
